526be424e724ba61679d858450d387f327c45f83
JoeMartinBTC <joemartin@nena-celeste.de>
2025-01-01 20:23:35 +0000

    Restored to 'da144d52713f5df86fc9a76463bb7e01ca9da5bf'
    Replit-Restored-To:da144d52713f5df86fc9a76463bb7e01ca9da5bf

--- client/src/components/HoldingArea.tsx
+++ client/src/components/HoldingArea.tsx
@@ -153,7 +153,6 @@ const handleCreateTemplate = () => {
                       </SelectTrigger>
                       <SelectContent>
                         {[
-                          'bg-white',
                           'bg-rose-100',
                           'bg-pink-100',
                           'bg-fuchsia-100',
