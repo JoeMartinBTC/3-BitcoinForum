161bb65232d6fbcc77390908a90c01faf013911e
JoeMartinBTC <joemartin@nena-celeste.de>
2025-01-21 16:35:24 +0000

    Restored to '5309cf726810ed99f5a55b6c4a73e8ce728f0495'
    Replit-Restored-To:5309cf726810ed99f5a55b6c4a73e8ce728f0495

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -52,7 +52,7 @@ function Badge({ className, variant, ...props }: BadgeProps) {
 const VersionBadge = () => {
   return (
     <div className={versionBadgeVariants()}>
-      v0.8.7.beta
+      v0.8.1
     </div>
   )
 }
